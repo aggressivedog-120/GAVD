9634c426251be9ede36818fe86ec7dc1093f9711
aggressivedog-120 <84957372+aggressivedog-120@users.noreply.github.com>
2021-06-07 12:00:33 +0800

    Update GetRealData.py

--- TestOnRealData/GetRealData.py
+++ TestOnRealData/GetRealData.py
@@ -7,14 +7,9 @@ import uqer
 
 import TestOnRealData.futures_info as future_info
 
-client = uqer.Client(token='3463931d00f1a37acb1028ea5b8a7a5c4fb7f14a9819177c71ef3c7781bd770c')
+client = uqer.Client(token='token')
 
-#
-# b = uqer.DataAPI.MktMFutdGet(mainCon=u"1",contractMark=u"",contractObject=u"CU",
-#                              tradeDate=u"",startDate=u"20000101",endDate=u"20190101",
-#                              field=u"",pandas="1")
 
-save_path = 'E:\\Research Project\\stable_learning\\RealData'
 for ex in [future_info.ex_sh, future_info.ex_cf, future_info.ex_zz, future_info.ex_dl]:
     futures = list(ex.keys())
     for f in futures:
